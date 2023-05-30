ce594f7b190723efa9ed5d15c88673e65818738b
Bala-Pavani <shinchanpscmr@gmail.com>
2023-05-30 15:02:58 +0530

    html workouts

---
+++ JS_DAY1.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>JS</title>
+</head>
+<body></body>
+<h1>Java Script</h1>
+<button type="button" onclick="document.getElementById("demo").innerHTML=date()">click me</button>
+<p id="demo"></p>
+</body>
+</html>
